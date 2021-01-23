f9b5329115788366eaae4011913dfc84038dcda9
letter33 <seryong12@gmail.com>
2021-01-24 00:24:22 +0900

    Movie app complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Movies from './movies'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async() => {
+    const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    setMovies(movies)
+    setIsLoading(false)
+  }
+  useEffect(()=>{
+    getMovies()
+  })
+
+
+  return(
+    <section className='container'>
+      {isLoading 
+      ? <div className='loader'>
+        <span className='loader__text'>Loading...</span>
+      </div> 
+      : <div className='movies'>
+          {movies.map(movie=>{
+        return (
+            <Movies 
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary= {movie.summary}
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+            />
+           )})}
+        </div>}
+    </section>
+  )
 }
 
 export default App;
