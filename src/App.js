041621d319eb96d08bddcfc4bd266f23269d7524
letter33 <seryong12@gmail.com>
2021-01-24 14:27:11 +0900

    class 컴포넌트를 function컴포넌트로 변경

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import Movies from './movies'
-import './App.css'
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [movies, setMovies] = useState([])
-
-  const getMovies = async() => {
-    const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
-    setMovies(movies)
-    setIsLoading(false)
-  }
-  useEffect(()=>{
-    getMovies()
-  })
-
-
-  return(
-    <section className='container'>
-      {isLoading 
-      ? <div className='loader'>
-        <span className='loader__text'>Loading...</span>
-      </div> 
-      : <div className='movies'>
-          {movies.map(movie=>{
-        return (
-            <Movies 
-            key={movie.id}
-            id={movie.id}
-            year={movie.year}
-            title={movie.title}
-            summary= {movie.summary}
-            poster={movie.medium_cover_image}
-            genres={movie.genres}
-            />
-           )})}
-        </div>}
-    </section>
-  )
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,33 @@
+import React, {useEffect} from "react";
+
+// class Detail extends React.Component {
+//   componentDidMount() {
+//     const { location, history } = this.props;
+//     if (location.state === undefined) {
+//       history.push("/");
+//     }
+//   }
+//   render() {
+//     const { location } = this.props;
+//     if (location.state) {
+//       return <span>{location.state.title}</span>;
+//     } else {
+//       return null;
+//     }
+//   }
+// }
+
+function Detail({location, history}) {
+  useEffect(()=>{
+    if(location.state===undefined) {
+      history.push('/')
+    }
+  })
+  if(location.state) {
+    return <span>{location.state.title}</span>
+  } else {
+    return  null
+  }
+}
+
+export default Detail;
